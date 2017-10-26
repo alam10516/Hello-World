5900162da9c2c174446688624ab8080424bf5346
alam10516 <33119215+alam10516@users.noreply.github.com>
2017-10-26 15:25:03 +0530

    Add files via upload
    This is a factorial program in c by reading argument line as input.

---
+++ fact.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+int fact(int);
+int main()
+{
+int n,m;
+gets(n);
+m=fact(n);
+puts(m);
+return 0;
+}
+int fact(int n)
+{
+if(n==0)
+return 1;
+else 
+return n*fact(n-1);
+}
+
+
+
+
+
+#include"stdio.h"
+
+int main(int argc, char *argv[])
+
+{ 
+
+    int n,f=1,i;     
+
+    n = atoi(argv[1]);    
+
+    for( i = 1; i <=n ; ++i ) 
+
+    {
+
+        f = f*i;
+
+    }
+
+    printf("%d",f);     
+
+    return 0;      
+
+}
